d09c7d475454c0a1f0a4199fa1dfd1cd58dd3e27
injiri <simoninjiri@gmail.com>
2020-10-09 23:57:08 +0300

    mpesa sdk setup at api level

---
+++ api/src/main/java/org/openmrs/module/mpesapay/Constants.java
@@ -0,0 +1,8 @@
+package org.openmrs.module.mpesapay;
+
+public class Constants {
+	
+	public static String APP_KEY = "GvzjNnYgNJtwgwfLBkZh65VPwfuKvs0V"; //replace with your app api key
+	
+	public static String APP_SECRET = "oOpJICRVlyrGSAkM"; //replace with your app api secret
+}
